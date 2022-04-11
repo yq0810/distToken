fa76aa23689bb869dc38d12b0da9685f07997722
yq0810 <yq.hong@whalechip.com>
2022-04-11 16:52:03 +0800

    upgrade

--- 1.bundle.js
+++ 1.bundle.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{336:function(n,w,o){"use strict";o.r(w)}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{340:function(n,w,o){"use strict";o.r(w)}}]);
